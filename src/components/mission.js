bf6ef88be483bd2f3fcd5e1319e3680cec9f2f30
J <80164315+jbolns@users.noreply.github.com>
2023-11-02 13:24:23 +0200

    Improved description > services

--- src/components/mission.js
+++ src/components/mission.js
@@ -15,13 +15,14 @@ const MissionComponent = () => {
                     <p><strong>To help build better and more sustainable digital technologies</strong>.</p>
                     <p className='reply'>How?</p>
                     <p>... We don't know the full answer to this question. No one does!</p>
-                    <p>That said, our long-term strategy is to make many small good things that add up to a toolkit for building digital technologies more ethically and more sustainably.</p>
+                    <p>That said, our long-term strategy is to make many small good things that add up to a toolkit for building digital technologies more ethically and more sustainably. We might also, in due course, think about certifying systems using these or equally-ethical/sustainable resources.</p>
                 </Col>
                 <Col className='col-10 offset-1 col-lg-5 offset-lg-0'>
                     <p className='reply'>And short-term, how can you help already today?</p>
                     <p>Two ways.<br />
-                        1. <strong>Check our resources.</strong> We are already producing resources addressing specific but very important challenges. These resources might already help you approach digital technologies better and more sustainably.</p>
-                    <p>2. <strong>Get in touch!</strong> We code alright. We can tailor solutions to your needs.</p>
+                        1. <strong>Check our resources.</strong> We are producing resources addressing specific but very important challenges – these might already help you approach digital technologies better and more sustainably.</p>
+                    <p>2. <strong>Check our services.</strong> We code (or help you code) in a way that considers resource-utilisation across the entire chain (server energy, CO2, data requirements, etc.). This helps build solutions that perform, are cost-efficient, and keep society and the environment in mind.</p>
+                    <p>Do <a href='mailto: hello@polyzentrik.com?SUBJECT=Hi!' target='_blank' rel='noreferrer' aria-label='Link to send email'><strong>get in touch</strong></a> for more details.</p>
                 </Col>
             </Row >
         </Container >
