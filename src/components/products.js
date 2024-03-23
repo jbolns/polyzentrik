643109197da2dad3c77b32eefd241ddb545440a8
J <80164315+jbolns@users.noreply.github.com>
2024-03-23 15:09:43 +0200

    Troubleshooting Gatsby Build Action Attempt 5

--- src/components/products.js
+++ src/components/products.js
@@ -36,42 +36,42 @@ const ProductsComponent = () => {
     <Container fluid className='horizontal-section shadow bg-highlight'>
       <Row>
         <Col className='col-lg-6 offset-lg-3 lokal'>
+        <StaticImage src='../images/graphics/lklbanner.png' alt='A banner logo of LOKAL' />
           <Container className='d-md-flex flex-wrap'>
-                <StaticImage src='..\images\graphics\LOKAL-banner.webp' alt='A banner logo of LOKAL' className='no-effect' />
-                  {
-                    posts.map(node => (
-                      <Card key={node.id} variant='top' className='h-100 mt-1' data-sal='slide-up' data-sal-delay='400' data-sal-easing='ease'>
-                        <Card.Body>
-                          <Card.Title data-sal='zoom-out' data-sal-delay='400' data-sal-easing='ease'>
-                            <h4>
-                              <Link to={`/products/${node.frontmatter.slug}`}>
-                                {node.frontmatter.title}
-                              </Link></h4>
-                          </Card.Title>
-                          <Card.Text>
-                            <Link to={`/products/${node.frontmatter.categories}`}>
-                              <span className='cats highlight'>
-                                {node.frontmatter.categories.replace('-', ' ')}
-                              </span>
-                            </Link>
-                            <span className='clearer excerpt'>{node.frontmatter.intro}</span>
-                            <span className='additional'>
-                              <span className='status'>License:&nbsp;</span>
-                              <span className={node.frontmatter.license}>
-                                {node.frontmatter.license}
-                              </span>.
-                            </span>
-                            <span className='clearer'></span>
-                            <Link to={`/products/${node.frontmatter.slug}`}>
-                              <Button className='more float-end'>
-                                Check product
-                              </Button>
-                            </Link>
-                          </Card.Text>
-                        </Card.Body>
-                      </Card>
-                    ))
-                  }
+            {
+              posts.map(node => (
+                <Card key={node.id} variant='top' className='h-100 mt-1' data-sal='slide-up' data-sal-delay='400' data-sal-easing='ease'>
+                  <Card.Body>
+                    <Card.Title data-sal='zoom-out' data-sal-delay='400' data-sal-easing='ease'>
+                      <h4>
+                        <Link to={`/products/${node.frontmatter.slug}`}>
+                          {node.frontmatter.title}
+                        </Link></h4>
+                    </Card.Title>
+                    <Card.Text>
+                      <Link to={`/products/${node.frontmatter.categories}`}>
+                        <span className='cats highlight'>
+                          {node.frontmatter.categories.replace('-', ' ')}
+                        </span>
+                      </Link>
+                      <span className='clearer excerpt'>{node.frontmatter.intro}</span>
+                      <span className='additional'>
+                        <span className='status'>License:&nbsp;</span>
+                        <span className={node.frontmatter.license}>
+                          {node.frontmatter.license}
+                        </span>.
+                      </span>
+                      <span className='clearer'></span>
+                      <Link to={`/products/${node.frontmatter.slug}`}>
+                        <Button className='more float-end'>
+                          Check product
+                        </Button>
+                      </Link>
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              ))
+            }
           </Container>
         </Col>
       </Row>
